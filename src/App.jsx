8bb2535da81d1827f5d026ac27cf48a573568d2f
Tubha Ahmad <toubha.ah@hotmail.com>
2026-06-02 20:27:29 +0200

    initial setup: React + Tailwind + React Router

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-bg min-h-screen flex items-center justify-center">
+      <h1 className="text-dark text-6xl font-bold">Toubha</h1>
+    </div>
+  );
+}
+
+export default App;
